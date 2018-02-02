8322c0ffe55eb8f40d399313e8a8e7f1bc3518c1
JDSalisbury <salisbury.jeffery@gmail.com>
2018-02-02 16:41:52 -0500

    about to work on feeding

--- src/test/java/shelter/ShelterTest.java
+++ src/test/java/shelter/ShelterTest.java
@@ -10,37 +10,38 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class ShelterTest {
-	
+
 	private VirtualPet newPet;
-	
+
 	@Test
 	public void shouldAddNewPetToShelter() {
 		PetShelter underTest = new PetShelter();
-		VirtualPet newPet = new VirtualPet("Bob","",0,0,0);
+		VirtualPet newPet = new VirtualPet("Bob", "", 0, 0, 0);
 		underTest.admitPet(newPet);
 		Collection<VirtualPet> check = underTest.petList();
 		assertThat(check, contains(newPet));
-		
+
 	}
 
 	@Test
 	public void shouldAddNewTwoPetToShelter() {
 		PetShelter underTest = new PetShelter();
-		VirtualPet newPet = new VirtualPet("Bob","",0,0,0);
-		VirtualPet newPet2 = new VirtualPet("Frank","",0,0,0);
+		VirtualPet newPet = new VirtualPet("Bob", "", 0, 0, 0);
+		VirtualPet newPet2 = new VirtualPet("Frank", "", 0, 0, 0);
 		underTest.admitPet(newPet);
 		underTest.admitPet(newPet2);
 		Collection<VirtualPet> check = underTest.petList();
-		assertThat(check, contains(newPet,newPet2));
+		assertThat(check, contains(newPet, newPet2));
 	}
+
 	@Test
 	public void shouldRemoveNewPetFromShelter() {
 		PetShelter underTest = new PetShelter();
-		VirtualPet newPet = new VirtualPet("Bob","",0,0,0);
+		VirtualPet newPet = new VirtualPet("Bob", "", 0, 0, 0);
 		underTest.adoptPet(newPet.getName());
 		underTest.findAnimal("Bob");
 		VirtualPet check = underTest.findAnimal("Bob");
 		assertThat(check, is(nullValue()));
 	}
-	
+
 }
